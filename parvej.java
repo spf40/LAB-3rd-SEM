a25369f337ccba129c28ab783e62253300745b6d
spf40 <mdparvejsheikh4062@gmail.com>
2025-02-13 13:26:05 +0600

    Add files via upload

---
+++ parvej.java
@@ -0,0 +1,13 @@
+class parvej {
+    int a =94 ;
+    void dis (int a){
+        System.out.println(a);
+    }
+            void dis2 (){
+                System.out.println(a);
+            }                 public static void main(String[] args){
+                    parvej ob= new parvej();
+                    ob.dis(90);
+                }
+            }
+        
