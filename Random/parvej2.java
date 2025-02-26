73784425d8110b31f71dec84415293752b46ad8c
spf40 <mdparvejsheikh4062@gmail.com>
2025-02-26 13:22:26 +0600

    Add files via upload

---
+++ Random/parvej2.java
@@ -0,0 +1,11 @@
+class parvej2 {
+    int a;
+    void show (){
+        scanner intput =new scanner (system.in);
+        a= input.next ln t ();
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+        parvej2 ob= new parvej1 ();
+        ob.diu ();
+}
