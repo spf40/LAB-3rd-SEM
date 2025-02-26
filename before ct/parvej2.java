d489633d09351119b6f9b34d764f62afcee8c30e
spf40 <mdparvejsheikh4062@gmail.com>
2025-02-26 14:13:36 +0600

    Add files via upload

---
+++ before ct/parvej2.java
@@ -0,0 +1,12 @@
+
+class parvej2 {
+    int a;
+    void show (){
+        scanner intput = new scanner (system.in);
+        a = input.nextlnt ();
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+        parvej2 ob= new parvej2 ();
+        ob.show();
+}
